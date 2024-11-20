5fbf28466b40729457cb1917abe5c5de712555e1
z.gerke@mail.ru <zange@LAPTOP-65G985TK>
2024-11-20 15:41:45 +0200

    l,

--- project test/project_test.py
+++ project test/project_test.py
@@ -1,3 +1,8 @@
 print('tere tulemast')
 nimi =input("Mis on sinu nimi? ")
-print('Tere tulemast!', nimi)
+print('Tere tulemast! Tervitan sind', nimi)
+print('Tere tulemast! Tervitan sind'+ nimi)
+vanus=int(input('kui vana sa oled? '))
+print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,'aastat vana')
+print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
+
