712861f04528242ebebcb898861f7a1c0b7629de
z.gerke@mail.ru <zange@LAPTOP-65G985TK>
2024-11-20 16:35:23 +0200

    end of lesson

--- project test/project_test.py
+++ project test/project_test.py
@@ -1,5 +1,9 @@
-﻿# print('tere tulemast')
-# nimi =input("Mis on sinu nimi? ")
+﻿from random import * # * - все фунции или можно написать только функцию если хочешь переименовать фунцию можно использовать
+# randint as rd - examlpleэ
+from math import *
+#1
+# print('tere tulemast')
+# nimi =input("Mis on sinu nimi? ").capitalize() #lower(), upper(), /
 # print('Tere tulemast! Tervitan sind', nimi)
 # print('Tere tulemast! Tervitan sind'+ nimi)
 # vanus=int(input('kui vana sa oled? '))
@@ -7,8 +11,20 @@
 # print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
 
 #2
-vanus = 18
-eesnimi = "Jaak"
-pikkus = 16.5
-kas_käib_koolis = True
-print(type(vanus)), print(type(eesnimi)), print(type(pikkus)), print(type(kas_käib_koolis))
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(type(vanus)), print(type(eesnimi)), print(type(pikkus)), print(type(kas_käib_koolis))
+
+#3
+# kokku=randint(1,1000)
+# print(kokku)
+# kommi=int(input('mittu kommi'))
+# kokku=kokku-kommi
+# print(f'jaak on {kokku} kommi')
+
+#4
+l = int(input('umbermoot'))
+d=l/pi
+print(round(d, 2))
