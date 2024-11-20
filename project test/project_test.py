23da56d3adc46e02805e177c2684bc2142f69142
z.gerke@mail.ru <zange@LAPTOP-65G985TK>
2024-11-20 16:00:20 +0200

    test 555

--- project test/project_test.py
+++ project test/project_test.py
@@ -1,8 +1,14 @@
-print('tere tulemast')
-nimi =input("Mis on sinu nimi? ")
-print('Tere tulemast! Tervitan sind', nimi)
-print('Tere tulemast! Tervitan sind'+ nimi)
-vanus=int(input('kui vana sa oled? '))
-print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,'aastat vana')
-print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
+﻿# print('tere tulemast')
+# nimi =input("Mis on sinu nimi? ")
+# print('Tere tulemast! Tervitan sind', nimi)
+# print('Tere tulemast! Tervitan sind'+ nimi)
+# vanus=int(input('kui vana sa oled? '))
+# print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,'aastat vana')
+# print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
 
+#2
+vanus = 18
+eesnimi = "Jaak"
+pikkus = 16.5
+kas_käib_koolis = True
+print(type(vanus)), print(type(eesnimi)), print(type(pikkus)), print(type(kas_käib_koolis))
