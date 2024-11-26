c6392e9a80d69188fd47d30d3576dbe4002abeaa
zhan-gabriel-gerke <z.gerke@mail.ru>
2024-11-26 10:31:24 +0200

    from 5 till 10 ex

--- project test/project_test.py
+++ project test/project_test.py
@@ -25,6 +25,40 @@ from math import *
 # print(f'jaak on {kokku} kommi')
 
 #4
-l = int(input('umbermoot'))
-d=l/pi
-print(round(d, 2))
+# l = int(input('umbermoot'))
+# d=l/pi
+# print(round(d, 2))
+
+#5
+N = int(input("Введите N"))
+M = int(input("Введите M"))
+S = N*M
+print(S, 'Длинна прямоугольного участка')
+
+#6 
+
+aeg = float(input("Mitu tundi kulus sõiduks? "))
+teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+kiirus = teepikkus / aeg # тут поменять местами надо было
+print("Sinu kiirus oli " + str(kiirus) + " km/h")
+
+#7
+
+N1, N2, N3, N4, N5 = int(input('Введите число 1')), int(input('Введите число 2')), int(input('Введите число 3')), int(input('Введите число 4')), int(input('Введите число 5'))
+print((N1 + N2 + N3 + N4 + N5) / 5, "среднее")
+
+#8
+
+print('    @..@\n   (----)\n  ( \\__/ )\n  ^^ «» ^^  ')
+
+#9 
+print('Вычисляем периметр треугольника введи все данные')
+A, B, C = int(input('A')), int(input('B')), int(input('B'))
+print('P = ',A + B + C)
+
+#10
+
+price = float(12.90)
+tips = float(0.10)
+costforperson = price * tips + price
+print('total cost :', round(costforperson, 3), 'euro, per person ', round(costforperson, 3) / 2, ' euro')
