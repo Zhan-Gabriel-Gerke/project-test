d16d97c3c1ca57dbb4319b43145fca579da4a15d
z.gerke@mail.ru <zange@LAPTOP-65G985TK>
2024-11-30 21:45:07 +0200

    finished

--- Programmeerimise alused/Programmeerimise alused/Iseseisev too Vigade otsing -1.py
+++ Programmeerimise alused/Programmeerimise alused/Iseseisev too Vigade otsing -1.py
@@ -1,1 +1,32 @@
-#test
+﻿#3 9, 12, 4.24
+## 5 4, 20.0, 18.0, 6.4
+### 10, 20.0, 314.16, 62.83
+from math import * # тут местами слова поменяли
+print("Ruudu karakteristikud")
+a=float(input('Sisesta ruudu külje pikkus => ')) # тут float не было
+S=a**2
+print("Ruudu pindala", S)
+P=4*a
+print("Ruudu ümbermõõt", P)# тут не те скобки были
+di = a * sqrt(2)#math не нужен, * не было
+print(di)
+print("Ruudu diagonaal", round(di,2))
+print()
+print("Ristküliku karakteristikud")# лишняя сокбка
+b=float(input("Sisesta ristküliku 1. külje pikkus => "))# float потерялся 
+c=float(input("Sisesta ristküliku 2. külje pikkus => "))# float потерялся
+S=b*c
+print('Ristküliku pindala', S)# ' не было
+P=2*(b+c) # * не было
+print("Ristküliku ümbermõõt", P)
+di = sqrt(b**2+c**2) # убрал math и добавил *
+print("Ristküliku diagonaal", round(di, 2)) # ) не было ну и 2 в округление поставил
+print()
+print("Ringi karakteristikud")
+r=float(input('Sisesta ringi raadiusi pikkus => '))# float не было и много '
+d=2*r# * не было
+print("Ringi läbimõõt", d)# , не было
+S=pi*r**2 # после pi () лишние, * не хватало для возведения в степерь
+print("Ringi pindala", round(S, 2))# 2 в округление добавил
+C=2*pi*r# * не было, после pi () лишние
+print("Ringjoone pikkus", round(C, 2))# ) не было, 2 в округление добавил
