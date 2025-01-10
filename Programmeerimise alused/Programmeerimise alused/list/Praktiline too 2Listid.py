935276ba87fe2ac6d39b119ec9a411cfafdd904f
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-01-10 22:42:54 +0200

    1

---
+++ Programmeerimise alused/Programmeerimise alused/list/Praktiline too 2Listid.py
@@ -0,0 +1,4 @@
+#1
+pfrace = input("enter phrace")
+glastniii = ['A', 'E', 'I', 'O', 'U']
+soglastnii = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
