a400e1f04523b50dab61e75bc81540a5688ef8e9
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-01-11 22:31:58 +0200

    11/1/25

--- Programmeerimise alused/Programmeerimise alused/list/Praktiline too 2Listid.py
+++ Programmeerimise alused/Programmeerimise alused/list/Praktiline too 2Listid.py
@@ -1,4 +1,20 @@
-#1
+#1 doesn't work
 pfrace = input("enter phrace")
-glastniii = ['A', 'E', 'I', 'O', 'U']
-soglastnii = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
+countVowels = 0
+countNonVowels = 0
+vowels = ['A', 'E', 'I', 'O', 'U']
+NonVowels = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
+lenpfrace = len(pfrace)
+for char in pfrace:
+    if char in vowels == True:
+        countVowels+=1
+        print('vowel')
+    elif char in NonVowels == True:
+        countNonVowels+=1
+        print('notvowel')
+# for char in pfrace.upper():
+#     if char in vowels():
+#         countVowels+=1
+#     elif char in NonVowels:
+#         countNonVowels+=1
+print(countVowels, countNonVowels)
