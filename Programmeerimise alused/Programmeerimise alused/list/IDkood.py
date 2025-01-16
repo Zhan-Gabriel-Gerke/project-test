12d711cb45a8cfd8dfaa1a38a90cba8c46ee2e6e
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-01-16 21:59:43 +0200

    Finished script for kontroll sum

--- Programmeerimise alused/Programmeerimise alused/list/IDkood.py
+++ Programmeerimise alused/Programmeerimise alused/list/IDkood.py
@@ -1,8 +1,14 @@
 ﻿IDkood = None
 FirstNumber = ['1','2','3','4','5','6']
 FirstNumbertest = ['1','2','3','4']
+Place = ['Kuressaare haigla', 'Tartu Ülikooli Naistekliinik', 'Ida-Tallinna keskhaigla, Pelgulinna sünnitusmaja (Tallinn)', 'Keila haigla', 'Rapla haigla, Loksa haigla, Hiiumaa haigla (Kärdla)', 'Ida-Viru keskhaigla (Kohtla-Järve, endine Jõhvi)', 'Maarjamõisa kliinikum (Tartu), Jõgeva haigla', 'Narva haigla', 'Pärnu haigla', 'Haapsalu haigla', 'Järvamaa haigla (Paide)', 'Rakvere haigla, Tapa haigla', 'Valga haigla', 'Viljandi haigla', 'Lõuna-Eesti haigla (Võru), Põlva haigla']
 ikoodid = []
 arvud = []
+quitvariable = input("Write Q if you want end programm")
+if quitvariable == 'Q' or 'q':
+    IDkood = 1
+else:
+    print('ffff') # Закончи момент с выходом и отсортируй списки
 while True:
     while IDkood is None:
         KontrollSumTest = 0
@@ -31,7 +37,6 @@ while True:
                                        NumbersTest = IDkoodtest[9]
                                        NumbersTest = int(NumbersTest)
                                        KontrollSumTest = KontrollSumTest + NumbersTest
-                                       print('Kontroll Sum: ', KontrollSumTest)
                                elif lastnumberID == 0:
                                    for x in range(8):
                                        NumbersTest = IDkoodtest[x-1]
@@ -42,27 +47,73 @@ while True:
                                            NumbersTest = IDkoodtest[x+6]
                                            NumbersTest = int(NumbersTest)
                                            KontrollSumTest = (NumbersTest * x) + KontrollSumTest
-                                       else:
-                                           print('Kontroll sum II Iteration', KontrollSumTest)
                                remainderOfKontrollSum = float(KontrollSumTest % 11)
                                if lastnumberID == remainderOfKontrollSum:
                                    ikoodid.append(IDkoodtest)
-                                   print(ikoodid)
-                                   print(f'It\'s {})
+                                   birthplace = IDkoodtest[7:10]
+                                   birthplace = int(birthplace)
+                                   if birthplace >= 1 and birthplace < 11:
+                                       birthplace = Place[0]
+                                   elif birthplace >= 11 and birthplace < 21:
+                                       birthplace = Place[1]
+                                   elif birthplace >= 21 and birthplace < 151:
+                                       birthplace = Place[2]
+                                   elif birthplace >= 151 and birthplace < 161:
+                                       birthplace = Place[3]
+                                   elif birthplace >= 161 and birthplace < 221:
+                                       birthplace = Place[4]
+                                   elif birthplace >= 221 and birthplace < 271:
+                                       birthplace = Place[5]
+                                   elif birthplace >= 271 and birthplace < 371:
+                                       birthplace = Place[6]
+                                   elif birthplace >= 371 and birthplace < 421:
+                                       birthplace = Place[7]
+                                   elif birthplace >= 421 and birthplace < 471:
+                                       birthplace = Place[8]
+                                   elif birthplace >= 491 and birthplace < 521:
+                                       birthplace = Place[9]
+                                   elif birthplace >= 521 and birthplace < 571:
+                                       birthplace = Place[10]
+                                   elif birthplace >= 571 and birthplace < 601:
+                                       birthplace = Place[11]
+                                   elif birthplace >= 601 and birthplace < 651:
+                                       birthplace = Place[12]
+                                   elif birthplace >= 651 and birthplace < 701:
+                                       birthplace = Place[13]
+                                   else:
+                                       print('Error Birth Place')
+                                       arvud.append(IDkoodtest)
+                                   sex = IDkoodtest[0]
+                                   sex = int(sex)
+                                   if sex % 2 == 0:
+                                       sex = 'Female'
+                                   else:
+                                       sex = 'Male'
+                                   if IDkoodtest[0] == '3' or IDkoodtest[0] == '4':
+                                       DateBirth = IDkoodtest[5:7] + '.' + IDkoodtest[3:5] + '.' + '19' + IDkoodtest[1:3]
+                                   elif IDkoodtest[0] == '5' or IDkoodtest[0] == '6':
+                                       DateBirth = IDkoodtest[5:7] + '.' + IDkoodtest[3:5] + '.' + '20' + IDkoodtest[1:3]
+                                   print(f"It's {sex} Date of Birthday {DateBirth} Place of Birth {birthplace}")
+
                                else:
-                                   print('Error aadddd')
-                               print(lastnumberID, 'ostaton ->', remainderOfKontrollSum)
+                                   print('Error Kontroll sum')
+                                   arvud.append(IDkoodtest)
                            else:
                                print('Error 6-7 numbers')
+                               arvud.append(IDkoodtest)
                        else:
                            print('Error 4-5 numbers')
+                           arvud.append(IDkoodtest)
                    else:
                        print('Error 2-3 numbers')
+                       arvud.append(IDkoodtest)
                 else:
                     print(IDkoodtest[0])
                     print('ID kood only can start form ', FirstNumber )
+                    arvud.append(IDkoodtest)
             else:
                 print('ID kood is not correct 11 numbers')
+                arvud.append(IDkoodtest)
         else:
             print('ID kood is not correct ONLY INT')
     
