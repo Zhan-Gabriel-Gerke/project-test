8973c533a903517199665f02f8e2425bf92c33ad
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-01-16 19:22:25 +0200

    save

--- Programmeerimise alused/Programmeerimise alused/list/IDkood.py
+++ Programmeerimise alused/Programmeerimise alused/list/IDkood.py
@@ -1,33 +1,38 @@
-﻿while True:
-    IDkood = None
-    FirstNumber = ['1','2','3','4','5','6']
-    FirstNumberstest = ['1','2','3','4']
-    KontrollSumTest = 0
+﻿IDkood = None
+FirstNumber = ['1','2','3','4','5','6']
+FirstNumbertest = ['1','2','3','4']
+ikoodid = []
+arvud = []
+while True:
     while IDkood is None:
+        KontrollSumTest = 0
         IDkoodtest = input('Enter ID kood: ')
         if IDkoodtest.isdigit() == True:
             if len(IDkoodtest) == 11:
                 if IDkoodtest[0] in FirstNumber:
                    NumbersTest = IDkoodtest[1:3]
-                   NumbersTest = int(NumbersTest)# Тут проблема снизу на 13 сторке 37605030270 # Тут список в списке без цикла ищётся сделай переменную и норм будет 
-                   if (NumbersTest >= 0 and NumbersTest < 26) or (NumbersTest >= 0 and NumbersTest < 100 and FirstNumber in FirstNumberstest):
+                   NumbersTest = int(NumbersTest)# Тут проблема снизу на 13 сторке 37605030270 # Тут список в списке без цикла ищётся сделай переменную и норм будет
+                   FirstNumbervariable = IDkoodtest[0]
+                   if (NumbersTest >= 0 and NumbersTest < 26) or (NumbersTest >= 0 and NumbersTest < 100 and FirstNumbervariable in FirstNumbertest):
                        NumbersTest = IDkoodtest[3:5]
                        NumbersTest = int(NumbersTest)                      
                        if NumbersTest > 0 and NumbersTest < 13:
                            NumbersTest = IDkoodtest[5:7]
                            NumbersTest = int(NumbersTest)
                            if NumbersTest > 0 and NumbersTest < 32:
-                               for x in range(10):
-                                   NumbersTest = IDkoodtest[x-1]
-                                   NumbersTest = int(NumbersTest)
-                                   KontrollSumTest = (NumbersTest * x) + KontrollSumTest
-                               else:
-                                   NumbersTest = IDkoodtest[9]
-                                   NumbersTest = int(NumbersTest)
-                                   KontrollSumTest = KontrollSumTest + NumbersTest
-                                   print('Kontroll Sum: ', KontrollSumTest)
-                               if KontrollSumTest % 11 == 10:
-                                   KontrollSumTest = 0
+                               lastnumberID = IDkoodtest[10]
+                               lastnumberID = int(lastnumberID)
+                               if lastnumberID != 0:
+                                   for x in range(10):
+                                       NumbersTest = IDkoodtest[x-1]
+                                       NumbersTest = int(NumbersTest)
+                                       KontrollSumTest = (NumbersTest * x) + KontrollSumTest
+                                   else:
+                                       NumbersTest = IDkoodtest[9]
+                                       NumbersTest = int(NumbersTest)
+                                       KontrollSumTest = KontrollSumTest + NumbersTest
+                                       print('Kontroll Sum: ', KontrollSumTest)
+                               elif lastnumberID == 0:
                                    for x in range(8):
                                        NumbersTest = IDkoodtest[x-1]
                                        NumbersTest = int(NumbersTest)
@@ -38,7 +43,15 @@
                                            NumbersTest = int(NumbersTest)
                                            KontrollSumTest = (NumbersTest * x) + KontrollSumTest
                                        else:
-                                           print('Kontroll sum II', KontrollSumTest)
+                                           print('Kontroll sum II Iteration', KontrollSumTest)
+                               remainderOfKontrollSum = float(KontrollSumTest % 11)
+                               if lastnumberID == remainderOfKontrollSum:
+                                   ikoodid.append(IDkoodtest)
+                                   print(ikoodid)
+                                   print(f'It\'s {})
+                               else:
+                                   print('Error aadddd')
+                               print(lastnumberID, 'ostaton ->', remainderOfKontrollSum)
                            else:
                                print('Error 6-7 numbers')
                        else:
