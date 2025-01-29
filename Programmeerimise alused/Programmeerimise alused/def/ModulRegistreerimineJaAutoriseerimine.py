2642597e093aac7d5d4768d68e72e0fe0f8149ce
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-01-29 11:15:28 +0200

    save

--- Programmeerimise alused/Programmeerimise alused/def/ModulRegistreerimineJaAutoriseerimine.py
+++ Programmeerimise alused/Programmeerimise alused/def/ModulRegistreerimineJaAutoriseerimine.py
@@ -1,1 +1,28 @@
-## modul
+﻿## modul
+from curses.ascii import isdigit, islower
+import random
+UsernamesList = []
+PasswordsList = []
+charsList = ['.',',',':',';','!','_','*','-','+','(',')','/','#','¤','%','&','@']
+print(charsList)
+def RandomPass():
+    str0=".,:;!_*-+()/#¤%&"
+    str1 = '0123456789'
+    str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    str3 = str2.upper()
+    str4 = str0+str1+str2+str3
+    ls = list(str4)
+    random.shuffle(ls)
+    # Извлекаем из списка 12 произвольных значений
+    psword = ''.join([random.choice(ls) for x in range(12)])
+    # Пароль готов
+    return psword
+
+def CheckPass(psword):
+    if psword.isdigit() == True:
+        if psword.isupper() == True:
+            if psword.islower() == True:
+                for x in range(len(psword)):
+                    if psword[x] in charsList:
+                        return True
+    return False
---
+++ Programmeerimise alused/test.py
@@ -0,0 +1,16 @@
+﻿charsList = ['.',',',':',';','!','_','*','-','+','(',')','/','#','¤','%','&','@']
+psword = input('ps')
+def CheckPass(psword):
+    for x in range(len(psword)):
+        if psword[x].isdigit() == True:
+            print('1 lvl')
+            if psword[x].isupper() == True:
+                print('2 lvl')
+                if psword[x].islower() == True:
+                    print('3 lvl')
+                    for y in range(len(psword)):
+                        if psword[y] in charsList:
+                            return True
+        return False
+Test=CheckPass(psword)
+print(Test)
