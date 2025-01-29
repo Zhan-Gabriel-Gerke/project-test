99fbf7176e2e00ebb5af90cd1dc375becab67758
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-01-29 17:36:20 +0200

    Created main fail

--- Programmeerimise alused/Programmeerimise alused/def/RegistreerimineJaAutoriseerimine.py
+++ Programmeerimise alused/Programmeerimise alused/def/RegistreerimineJaAutoriseerimine.py
@@ -1,2 +1,20 @@
 from ModulRegistreerimineJaAutoriseerimine import *
-a = input('Sign up - 1, Log In - 2, Change Password or Username - 3, Forgot password - 4: ')
+a = input('Sign in - 1, Log in - 2, Change Password or Username - 3, Forgot password - 4: ')
+if a == '1':
+    Username, Password = input('Username'), input('Password')
+    answer = Sign_in(Username, Password)
+elif a == '2':
+    Username, Password = input('Username'), input('Password')
+    answer = Log_in(Username, Password)
+elif a == '3':
+    b = input('Change username == 1, Change Password == 2')
+    if b == '1':
+        Username, NewUsername = input('Username'), input('New Username')
+        answer = Change_Username(Username, NewUsername)
+    elif b == '2':
+        Username, OldPass, NewPass = input('Username'), input('Old password'), input('New password')
+        answer = Change_Password(Username, OldPass, NewPass)
+elif a == '4':
+    Username = input('Username')
+    answer = Password_Reset(Username)
+print(answer) 
