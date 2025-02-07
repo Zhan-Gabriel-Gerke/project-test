647119594bb8bb5e4fd9483202c90a21fa3b372b
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-02-07 16:58:00 +0200

    1

--- Programmeerimise alused/Programmeerimise alused/Fail/TarkvaraarendajavastuvottMain.py
+++ Programmeerimise alused/Programmeerimise alused/Fail/TarkvaraarendajavastuvottMain.py
@@ -1,18 +1,17 @@
+from TarkvaraarendajavastuvottModul import *
+questions = []
+congratulations_name = []
+congratulations_score = []
+goodluck_list = []
+score = []
 while 1:
     try:
-        quantityofInterview = int(input('Enter how many interviews do you want to do'))
+        howMany = int(input('Enter how many interviews do you want to do'))
     except:
         print('Error')
-    if quantityofInterview < 5:
+    if howMany < 5:
         print('Min 5 interviews')
     else:
         break
-
-# for x in range(quantityofInterview):
-#     name = input('Enter name of candidate')
-#     f = open(r'C:\Users\LP1\source\repos\Zhan-Gabriel-Gerke\project-test\Programmeerimise alused\Programmeerimise alused\Fail\kusimused_vastused.txt', 'r', encoding ='utf-8-sig')
-#     questions_answers = []
-#     for row in f:
-#         questions_answers.append(row.strip())
-#     f.close()
-#     print(questions_answers)
+filePath = r'C:\Users\LP1\source\repos\Zhan-Gabriel-Gerke\project-test\Programmeerimise alused\Programmeerimise alused\Fail\kusimused_vastused.txt'
+questionsVar =
--- Programmeerimise alused/Programmeerimise alused/Fail/TarkvaraarendajavastuvottModul.py
+++ Programmeerimise alused/Programmeerimise alused/Fail/TarkvaraarendajavastuvottModul.py
@@ -1,7 +1,9 @@
 import random
 questions = []
-congratulations_list = []
+congratulations_name = []
+congratulations_score = []
 goodluck_list = []
+score = []
 def read_file(fail:str)->list:
     f = open(fail, 'r', encoding="utf-8-sig")
     #variable = f.read().splitlines()
@@ -14,9 +16,36 @@ def read_file(fail:str)->list:
 ff = r'C:\Users\LP1\source\repos\Zhan-Gabriel-Gerke\project-test\Programmeerimise alused\Programmeerimise alused\Fail\kusimused_vastused.txt'
 answer1 = read_file(ff)
 # print(answer[1])
-def questioner(name:str, lines:list):
+def questioner(name:str, lines:list, congratulations_name:list, congratulations_score:list, goodluck_list:list):
     numbersOfQuestions = [0,1,2,3,4,5,6,7,8,9]
-    QuestionNumber = random.randint(0,9)
-    return QuestionNumber
-answer = questioner('LLL', answer1)
-print(answer)
+    count = 0
+    for x in range(5):
+        Bnumber = False 
+        while Bnumber == False:
+            QuestionNumber = random.randint(1,11)
+            if QuestionNumber in numbersOfQuestions:
+                Bnumber = True
+                numbersOfQuestions.remove(QuestionNumber)
+        Question, Answer = lines[QuestionNumber].split(": ")
+        print(name, 'Answer the question. Question NR:',Question)
+        answer = input('Enter answer: ').upper()
+        # print(Answer)
+        if Answer == answer:
+            count = count + 1
+            # print(count)
+    if count >= 3:
+        congratulations_name.append(name)
+        congratulations_score.append(count)
+    else:
+        goodluck_list.append(name, count)
+    return congratulations_name, congratulations_score, goodluck_list
+# answer = questioner('Zhan', answer1, congratulations_list, goodluck_list)
+howMany = int(input('How many interviwes will be?: '))
+for interviews in range(howMany):
+    name = input('Enter name of person')
+    answer = questioner(name, answer1, congratulations_name, congratulations_score, goodluck_list)
+def sortListWithGrade(congratulations_name:list, congratulations_score:list):
+    sortV = sorted(range(len(score)), key=lambda i: score[i], reverse=True)
+    sorted_congratulations_name = [congratulations_name[i] for i in sortV]
+    sorted_congratulations_score = [congratulations_score[i] for i in sortV]
+    return sorted_congratulations_name, sorted_congratulations_score
