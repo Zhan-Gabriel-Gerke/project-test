14601ed0420286c9d0ecbf0e8e589e3812d34712
Zhan-Gabriel-Gerke <zangerke3@gmail.com>
2025-02-26 09:02:56 +0200

    save1

---
+++ Programmeerimise alused/Programmeerimise alused/Grafika/quadHMgrafPack.py
@@ -0,0 +1,20 @@
+﻿from tkinter import *
+
+def aken():
+    aken=Tk()
+    aken.geometry("650x260")
+    aken.title("Ruutvorand")
+    f1=Frame(aken, width=650, height=260)
+    f1.pack
+    lbl=Label(f1, text="Ruutvorrandite lahendamine", font="Calibri 26", fg="green", bg="lightblue")
+    lbl.pack(side=TOP)
+    lbl_vastus=Label(f1, text="Lahendamine", height=4, width=60, bg="yellow")
+    lbl_vastus.pack(side=BOTTOM)
+    lbl_a=Entry(f1, font="Calibri 26", fg="green", bg="lightblue", width=3)
+    lbl_a.pack(side=LEFT)
+    x2=Label(f1, text="x^2", font="Calibri 26", fg="green", padx=10)
+    x2.pack(side=LEFT)
+    aken.mainloop()
+aken()
+
+# Не работает
